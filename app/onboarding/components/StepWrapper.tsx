a61829d5ad07b4633fe9d345f7fb604f494c623d
Darryl Barnes <dlbarnes@dbglobalinvestments.com>
2026-06-28 07:11:27 -0500

    Remove old onboarding flow causing build errors

--- app/onboarding/components/StepWrapper.tsx
+++
@@ -1,7 +0,0 @@
-export default function StepWrapper({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="bg-white shadow-lg rounded-xl p-8 border border-gray-200">
-      {children}
-    </div>
-  );
-}
